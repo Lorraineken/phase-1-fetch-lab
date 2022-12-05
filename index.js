879531150343bd4f165901880035366a6e795f76
Lorraineken <lorrainekupa@gmail.com>
2022-12-05 10:58:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  fetch('https://anapioficeandfire.com/api/books')
-    .then((response) => response.json())
-    .then((books) => renderBooks(books))
+ return (fetch('https://anapioficeandfire.com/api/books')
+         .then((response) => response.json())
+         .then((books) => renderBooks(books)))
 
   //.then(renderBooks(response.json))
 }
@@ -17,5 +17,5 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  fetchBooks();
+   fetchBooks();
 });
